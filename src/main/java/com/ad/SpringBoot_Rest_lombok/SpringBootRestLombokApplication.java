d4dedd1ac5d296650ab78d2ff05e779fb052fa3e
Ayesh Dulanja <ayeshdulanja@gmail.com>
2022-05-05 15:29:29 +0530

    .

---
+++ src/main/java/com/ad/SpringBoot_Rest_lombok/SpringBootRestLombokApplication.java
@@ -0,0 +1,13 @@
+package com.ad.SpringBoot_Rest_lombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestLombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestLombokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ad/SpringBoot_Rest_lombok/SpringBootRestLombokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ad.SpringBoot_Rest_lombok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestLombokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
